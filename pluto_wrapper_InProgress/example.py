4344092c4d9d87e61f2729f62209a3465dda036f
Prasanna-icefire <prasannamg3@gmail.com>
2023-02-28 13:38:07 +0530

    Made a few changes

---
+++ pluto_wrapper_InProgress/example.py
@@ -0,0 +1,12 @@
+from Pluto import pluto
+import time
+
+my_pluto = pluto()# This commands creates an object of pluto() where you can connect your hardware to. my_pluto is literally your plutodrone
+
+my_pluto.arm()# Arm your Pluto Drone
+time.sleep(5)
+
+my_pluto.disarm()# Disarm your pluto Drone
+exit()
+
+
